a7640299f8b8a7254d6e9183974f604a91c1b902
Izarothy <izarothmail@gmail.com>
2022-05-03 11:26:51 +0200

    Endpoint do rejestracji

--- Back/api.js
+++ Back/api.js
@@ -2,6 +2,11 @@ const express = require('express')
 const app = express()
 const port = 3000
 
-app.listen(port, () => {
-  console.log("Serwer na porcie" + port)
+app.post('/register', (req, res) => {
+  const registerEmail = req.body.email;
+  const registerPassword = req.body.password;
 })
+
+app.listen(port, () => {
+  console.log('Serwer na porcie' + port);
+});
