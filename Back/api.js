3f888a02eb8039e7295f7bebcecea4e1ea8ebbef
michaldudek11 <michal_dudek@zsgorzyce.edu.pl>
2022-05-09 12:15:01 +0200

    Endpoint do usuwania postów

--- Back/api.js
+++ Back/api.js
@@ -1,59 +1,92 @@
 // Zmienne env
-require('dotenv').config()
+require("dotenv").config();
 const DB_URL = process.env.DB_URL;
 
-// Mongodb 
-const mongoose = require('mongoose')
-const Advert = require('./Models/Advert')
-const User = require('./Models/User')
-mongoose.connect(DB_URL).catch((err) => console.error(err))
+// Mongodb
+const mongoose = require("mongoose");
+const Advert = require("./Models/Advert");
+const User = require("./Models/User");
+mongoose.connect(DB_URL).catch((err) => console.error(err));
 
-// Express 
-const express = require('express');
-const bodyParser = require('body-parser');
+// Express
+const express = require("express");
+const bodyParser = require("body-parser");
 const jsonParser = bodyParser.json();
-const cors = require('cors')
-const app = express()
-const port = 3000
-app.use(cors())
+const cors = require("cors");
+const app = express();
+const port = 3000;
+app.use(cors());
 
-app.post('/register', jsonParser, (req, res) => {
+app.post("/register", jsonParser, (req, res) => {
   const registerEmail = req.body.email;
   const registerPassword = req.body.password;
 
   const userToRegister = new User({
     email: registerEmail,
-    password: registerPassword
-  })
+    password: registerPassword,
+  });
 
   if (!registerEmail || !registerPassword) {
-    return res.status(400).json({"error": "Brakuje maila lub hasła"})
+    return res.status(400).json({ error: "Brakuje maila lub hasła" });
   }
 
   User.findOne({ email: registerEmail }, (err, user) => {
-    if (user) return res.status(400).json({status: "error", message: "Taki użytkownik już istnieje"})
+    if (user)
+      return res
+        .status(400)
+        .json({ status: "error", message: "Taki użytkownik już istnieje" });
 
-    userToRegister.save((err) => err && console.log(err))
-    res.status(200).json({status: "ok"})
-  })
+    userToRegister.save((err) => err && console.log(err));
+    res.status(200).json({ status: "ok" });
+  });
+});
 
-})
-
-app.post('/login', jsonParser, (req, res) => {
+app.post("/login", jsonParser, (req, res) => {
   const loginEmail = req.body.email;
   const loginPassword = req.body.password;
 
-  User.findOne({email: loginEmail}, (err, user) => {
-    if (err) return res.status(404).json({status: "error", message: "Błąd podczas logowania"})
+  User.findOne({ email: loginEmail }, (err, user) => {
+    if (err)
+      return res
+        .status(404)
+        .json({ status: "error", message: "Błąd podczas logowania" });
 
-    if (!user) return res.status(404).json({status: "error", message: "Nie ma użytkownika z takim mailem"})
+    if (!user)
+      return res.status(404).json({
+        status: "error",
+        message: "Nie ma użytkownika z takim mailem",
+      });
 
-    if (user.password !== loginPassword) return res.status(404).json({status: "error", message: "Nieprawidłowe hasło"})
+    if (user.password !== loginPassword)
+      return res
+        .status(404)
+        .json({ status: "error", message: "Nieprawidłowe hasło" });
 
-    res.status(200).json({status: "ok"})
+    res.status(200).json({ status: "ok" });
+  });
+});
+
+app.delete(
+  "/postDelete",
+  jsonParser,
+  (req,
+  (res) => {
+    const postID = req.body.postID || null;
+
+    if (!postID) return res.status(400).json({ status: "Nie podano ID" });
+
+    Advert.findOne({ _id: postID }, (advert, err) => {
+      if (!advert)
+        return res
+          .status(404)
+          .json({ status: "Nie znaleziono takiego ogłoszenia" });
+
+      Advert.deleteOne({ _id: postID });
+
+      res.status(200).json({ status: "ok" });
+    });
   })
-})
-
+);
 app.listen(port, () => {
-  console.log('Serwer na porcie ' + port);
+  console.log("Serwer na porcie " + port);
 });
