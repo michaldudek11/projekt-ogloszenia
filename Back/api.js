c33f1521775d1a0105c0d498b59cb0ecaf328297
michaldudek11 <michal_dudek@zsgorzyce.edu.pl>
2022-05-10 13:14:05 +0200

    Format api.js

--- Back/api.js
+++ Back/api.js
@@ -1,23 +1,23 @@
 // Zmienne env
-require("dotenv").config();
+require('dotenv').config();
 const DB_URL = process.env.DB_URL;
 
 // Mongodb
-const mongoose = require("mongoose");
-const Advert = require("./Models/Advert");
-const User = require("./Models/User");
+const mongoose = require('mongoose');
+const Advert = require('./Models/Advert');
+const User = require('./Models/User');
 mongoose.connect(DB_URL).catch((err) => console.error(err));
 
 // Express
-const express = require("express");
-const bodyParser = require("body-parser");
+const express = require('express');
+const bodyParser = require('body-parser');
 const jsonParser = bodyParser.json();
-const cors = require("cors");
+const cors = require('cors');
 const app = express();
 const port = 3000;
 app.use(cors());
 
-app.post("/register", jsonParser, (req, res) => {
+app.post('/register', jsonParser, (req, res) => {
   const registerEmail = req.body.email;
   const registerPassword = req.body.password;
 
@@ -27,70 +27,61 @@ app.post("/register", jsonParser, (req, res) => {
   });
 
   if (!registerEmail || !registerPassword) {
-    return res.status(400).json({ error: "Brakuje maila lub hasła" });
+    return res.status(400).json({ error: 'Brakuje maila lub hasła' });
   }
 
   User.findOne({ email: registerEmail }, (err, user) => {
     if (user)
-      return res
-        .status(400)
-        .json({ status: "error", message: "Taki użytkownik już istnieje" });
+      return res.status(400).json({ status: 'error', message: 'Taki użytkownik już istnieje' });
 
     userToRegister.save((err) => err && console.log(err));
-    res.status(200).json({ status: "ok" });
+    res.status(200).json({ status: 'ok' });
   });
 });
 
-app.post("/login", jsonParser, (req, res) => {
+app.post('/login', jsonParser, (req, res) => {
   const loginEmail = req.body.email;
   const loginPassword = req.body.password;
 
   User.findOne({ email: loginEmail }, (err, user) => {
-    if (err)
-      return res
-        .status(404)
-        .json({ status: "error", message: "Błąd podczas logowania" });
+    if (err) return res.status(404).json({ status: 'error', message: 'Błąd podczas logowania' });
 
     if (!user)
       return res.status(404).json({
-        status: "error",
-        message: "Nie ma użytkownika z takim mailem",
+        status: 'error',
+        message: 'Nie ma użytkownika z takim mailem',
       });
 
     if (user.password !== loginPassword)
-      return res
-        .status(404)
-        .json({ status: "error", message: "Nieprawidłowe hasło" });
+      return res.status(404).json({ status: 'error', message: 'Nieprawidłowe hasło' });
 
-    res.status(200).json({ status: "ok" });
+    res.status(200).json({ status: 'ok' });
   });
 });
 
-app.delete("/postDelete", jsonParser, (req, res) => {
+app.delete('/postDelete', jsonParser, (req, res) => {
   const postID = req.body.postID || null;
 
-  if (!postID) return res.status(400).json({ status: "Nie podano ID" });
+  if (!postID) return res.status(400).json({ status: 'Nie podano ID' });
 
   Advert.findOne({ _id: postID }, (advert, err) => {
     if (!advert) {
-      return res
-        .status(404)
-        .json({ status: "Nie znaleziono takiego ogłoszenia" });
+      return res.status(404).json({ status: 'Nie znaleziono takiego ogłoszenia' });
     }
 
     Advert.deleteOne({ _id: postID });
 
-    res.status(200).json({ status: "ok" });
+    res.status(200).json({ status: 'ok' });
   });
 });
 
-app.get("/getAllPosts", async (req, res) => {
+app.get('/getAllPosts', async (req, res) => {
   const adverts = await Advert.find({}).limit(20);
 
   res.status(200).send(adverts);
 });
 
-app.post("/addPost", jsonParser, async (req, res) => {
+app.post('/addPost', jsonParser, async (req, res) => {
   const { title, description, image, price, category } = req.body;
 
   const postToAdd = new Advert({
@@ -107,5 +98,5 @@ app.post("/addPost", jsonParser, async (req, res) => {
 });
 
 app.listen(port, () => {
-  console.log("Serwer na porcie " + port);
+  console.log('Serwer na porcie ' + port);
 });
