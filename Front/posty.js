b325f449eef900c55eda1bb6953b4d108a1bb72b
Izarothy <izarothmail@gmail.com>
2022-05-16 22:28:45 +0200

    Modal po stronie js, obsługa błędu z ogłoszeniami

--- Front/posty.js
+++ Front/posty.js
@@ -10,6 +10,7 @@ const renderPosts = async () => {
   const posts = await getAllPosts();
 
   posts.reverse().forEach((postData) => {
+    if (Object.values(postData).length < 5) return;
     const post = `
         <article class="kartaOgloszenie">
             ${postData.image.includes("http") ? `<img src=${postData.img} >` : `<img src="placeholder.png">`}
@@ -21,3 +22,35 @@ const renderPosts = async () => {
 };
 
 renderPosts();
+
+const openModal = () => {
+  const modal = document.querySelector('.modal');
+  modal.style.display = "flex";
+}
+
+const handleAdd = async (event) => {
+  event.preventDefault()
+  const addForm = document.querySelector('.modalForm');
+
+  const title = addForm.elements['title'].value;
+  const price = addForm.elements['price'].value;
+  const img = addForm.elements['img'].value;
+  const description = addForm.elements['description'].value;
+  const category = addForm.elements['category'].value;
+
+  const response = await fetch('http://localhost:3000/addPost', {
+    method: 'POST',
+    body: {
+      title: title,
+      price: price,
+      img: img,
+      description: description,
+      category: category}
+  })
+
+  if (response.ok) document.querySelector(".modal").style.display = "none"
+}
+
+const closeModal = () => {
+  document.querySelector('.modal').style.display = "none";
+}
