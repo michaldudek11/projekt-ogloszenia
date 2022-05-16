39b4748eeb78eee2e19677ded4664240f4bf4571
Izarothy <izarothmail@gmail.com>
2022-05-16 22:04:45 +0200

    Placeholder na zdjęcie, brak przesuwania na boki

--- Front/posty.js
+++ Front/posty.js
@@ -12,7 +12,7 @@ const renderPosts = async () => {
   posts.reverse().forEach((postData) => {
     const post = `
         <article class="kartaOgloszenie">
-            ${postData.image ? `<img src=${postData.img} >` : ``}
+            ${postData.image.includes("http") ? `<img src=${postData.img} >` : `<img src="placeholder.png">`}
             <h3>${postData.title}</h3>
             <h4>${postData.price} zł</h4>
         </article>`;
