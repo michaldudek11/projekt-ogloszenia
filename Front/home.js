b99ebfd9470c3c040aa202e3b3456c8fb0a7140f
Izarothy <izarothmail@gmail.com>
2022-05-17 11:45:45 +0200

    Wyświetlenie emailu użytkownika

--- Front/home.js
+++ Front/home.js
@@ -1,3 +1,8 @@
 if (!localStorage.getItem('email')) {
     window.location.href = 'http://localhost:5500/Front/logowanie.html';
 }
+
+const email = localStorage.getItem('email');
+
+const userName = document.querySelector(".userName")
+userName.innerText = `${userName.innerText} ${email}`
